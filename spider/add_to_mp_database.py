5d6200bacda16b6306c30b47263b50e869450243
wuwe1 <wu_we1@outlook.com>
2020-08-04 16:29:14 +0800

    add upload data to database

--- spider/add_to_mp_database.py
+++ spider/add_to_mp_database.py
@@ -2,41 +2,73 @@ import os
 import json
 import requests
 
+
 class MpDatabase():
-    ACCESS_TOKEN = ""
+    APPID = ""
+    APPSECRET = ""
     ENV = ""
 
     def __init__(self):
         pass
 
-    def databaseAdd(self):
+    def process_json_response(self, response):
+        response.raise_for_status()
+
+        j = response.json()
+        print(j)
+
+        errcode = j.get("errcode")
+        if errcode:
+            raise Exception(errcode)
+
+        return j
+
+    def getAccessToken(self):
+        url = f"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={self.APPID}&secret={self.APPSECRET}"
+
+        res = requests.get(url)
+        data = self.process_json_response(res)
+
+        self.ACCESS_TOKEN = data["access_token"]
+        
+
+
+    def databaseAdd(self, file_path):
+        if not os.path.exists(file_path) or not os.path.isfile(file_path):
+            raise Exception(f"check file: {file_path}")
+
+        if os.stat(file_path).st_size == 0:
+            raise Exception(f"Empty file: {file_path}")
+
         url = f'https://api.weixin.qq.com/tcb/databaseadd?access_token={self.ACCESS_TOKEN}'
         
-        with open("./items.jl") as jl:
+        with open(file_path) as jl:
             lines = []
             for line in jl.readlines():
                 lines.append(line)
             text = ','.join(lines)
         
-        
         query = '''
-        db.collection("test").add({
+        db.collection("news").add({
             data: [
                 ''' + text + '''
             ]
         })
         '''
+
         data = {
             "env": self.ENV,
             "query": query
         }
 
         res = requests.post(url, data=json.dumps(data))
-        res = json.loads(res.content.decode(encoding='utf-8'))
-        return res
+        j = self.process_json_response(res)
+
+        return j
         
 
 
-md = MpDatabase()
-res = md.databaseAdd()
-print(res)
+if __name__ == "__main__":
+    md = MpDatabase()
+    md.getAccessToken()
+    md.databaseAdd("./items.jl")
